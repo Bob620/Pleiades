012aa21fb5fe5b3c07a7468af9b7e185c0b8373d
bob620 <bruder.kraft225@gmail.com>
2018-02-05 01:19:17 -0600

    feat(httpserver) extracted adding and searching urls

--- HttpServer/Handler.go
+++ HttpServer/Handler.go
@@ -19,52 +19,37 @@ type Route struct {
 }
 
 func (handler Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	if req.RequestURI == "/" {
-		req.RequestURI = ""
-	}
-	req.ParseForm()
-	requestArray := strings.Split(strings.Split(req.RequestURI, "?")[0], "/")
+	route := handler.searchURL(req.RequestURI)
 
-	k := 0
-	var currentRoute Route
-	currentRouteList := handler.Routes
-
-	for k < len(requestArray) {
-		requestRoute := requestArray[k]
-		var broke = false
-		for i := range currentRouteList {
-			route := currentRouteList[i]
-			if route.URL == requestRoute {
-				currentRoute = route
-				currentRouteList = route.Routes
-				broke = true
-				break
-			}
-		}
-		if !broke {
-			currentRoute.Action(res, *req)
-			return
-		}
-		k++
-	}
-
-	currentRoute.Action(res, *req)
+	route.Action(res, *req)
 }
 
 func (handler *Handler) AddError(errorType string, f func(res http.ResponseWriter, req http.Request)) {
 	switch errorType {
 	case "404":
-		handler.RouteNotFound = Route{"404", "GET", []Route{}, f}
+		handler.RouteNotFound = Route{"404", http.MethodGet, []Route{}, f}
 	case "500":
-		handler.RouteNotFound = Route{"500", "GET", []Route{}, f}
+		handler.RouteNotFound = Route{"500", http.MethodGet, []Route{}, f}
 	}
 }
-/*
+
 func (handler *Handler) Delete(URL string, f func(res http.ResponseWriter, req http.Request)) {
-	handler.DeleteRoutes = append(handler.DeleteRoutes, Route{URL, f})
+	handler.addURL(URL, http.MethodDelete, f)
 }
-*/
+
 func (handler *Handler) Get(URL string, f func(res http.ResponseWriter, req http.Request)) {
+	handler.addURL(URL, http.MethodGet, f)
+}
+
+func (handler *Handler) Post(URL string, f func(res http.ResponseWriter, req http.Request)) {
+	handler.addURL(URL, http.MethodPost, f)
+}
+
+func (handler *Handler) Put(URL string, f func(res http.ResponseWriter, req http.Request)) {
+	handler.addURL(URL, http.MethodPut, f)
+}
+
+func (handler Handler) searchURL(URL string) Route {
 	if URL == "/" {
 		URL = ""
 	}
@@ -87,13 +72,44 @@ func (handler *Handler) Get(URL string, f func(res http.ResponseWriter, req http
 			}
 		}
 		if !broke {
-			var route Route;
+			return currentRoute
+		}
+		k++
+	}
+
+	return currentRoute
+}
+
+func (handler *Handler) addURL(URL string, method string, f func(res http.ResponseWriter, req http.Request)) {
+	if URL == "/" {
+		URL = ""
+	}
+	requestArray := strings.Split(URL, "/")
+
+	k := 0
+	var currentRoute Route
+	currentRouteList := handler.Routes
+
+	for k < len(requestArray) {
+		requestRoute := requestArray[k]
+		var broke = false
+		for i := range currentRouteList {
+			route := currentRouteList[i]
+			if route.URL == requestRoute {
+				currentRoute = route
+				currentRouteList = route.Routes
+				broke = true
+				break
+			}
+		}
+		if !broke {
+			var route Route
 			if k+1 == len(requestArray) {
-				route = Route{currentRoute.URL, "GET", []Route{}, f}
-			} else {
-				route = Route{requestRoute, "GET", []Route{}, func(res http.ResponseWriter, req http.Request) {
+				route = Route{currentRoute.URL, method, []Route{}, f}
+				route = Route{requestRoute, method, []Route{}, func(res http.ResponseWriter, req http.Request) {
 					handler.RouteNotFound.Action(res, req)
 				}}
+			} else {
 			}
 			if currentRoute.Action == nil {
 				handler.Routes = append(handler.Routes, route)
@@ -106,12 +122,3 @@ func (handler *Handler) Get(URL string, f func(res http.ResponseWriter, req http
 		k++
 	}
 }
-/*
-func (handler *Handler) Post(URL string, f func(res http.ResponseWriter, req http.Request)) {
-	handler.PostRoutes = append(handler.PostRoutes, Route{URL, f})
-}
-
-func (handler *Handler) Put(URL string, f func(res http.ResponseWriter, req http.Request)) {
-	handler.PutRoutes = append(handler.PutRoutes, Route{URL, f})
-}
-*/
